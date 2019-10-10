55e477e67136ba85563c5296eb4d803be03fc085
yusuke <denkikairo@gmail.com>
2019-10-10 12:33:58 +0900

    Fix: task_specの最初のキャプションのミスを修正

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :system do
+RSpec.describe 'Tasks', type: :system do
   describe 'タスクの新規作成、編集、削除' do
     context '正常系' do
       let(:user) { create(:user) }
