2c28e9356ed5548d3e38ef800acfb19e524dfd30
yusuke <denkikairo@gmail.com>
2019-10-11 15:39:30 +0900

    Fix: letをまとめる、task newでletを使わない

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,24 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe 'Tasks', type: :system do
+  let(:user) { create(:user) }
+  let(:user_another) { create(:user, email: 'another@gmail.com') }
+  let(:task){ create(:task, user_id: user.id) }
   describe 'タスクの新規作成画面' do
     context 'ログインした状態' do
-      let(:user) { create(:user) }
-      let(:task) { build(:task, title: 'task_new') }
-      let(:task_created){ create(:task, user_id: user.id) }
       it 'タスクが新規作成できる' do
         login_as(user)
         visit new_task_path
-        fill_in 'task_title', with: task.title
-        fill_in 'task_content', with: task.content
-        select task.status, from: 'task_status'
-        fill_in 'task_deadline', with: task.deadline
+        fill_in 'task_title', with: 'title'
+        fill_in 'task_content', with: 'content'
+        select 'todo', from: 'task_status'
+        fill_in 'task_deadline', with: '002019/10/9 11:11'
         click_button 'Create Task'
         expect(page).to have_content 'Task was successfully created.'
         visit tasks_path
-        expect(page).to have_content task.content
-        expect(page).to have_content task.status
-        expect(page).to have_content task.deadline.strftime('%Y/%-m/%-d %-H:%-M')
+        expect(page).to have_content 'content'
+        expect(page).to have_content 'todo'
+        expect(page).to have_content '2019/10/9 11:11'
       end
     end
     context 'ログインしていない状態' do
@@ -31,11 +31,9 @@ RSpec.describe 'Tasks', type: :system do
   end
   describe 'タスクの編集画面' do
     context 'ログインした状態' do
-      let(:user) { create(:user) }
-      let(:task_created){ create(:task, user_id: user.id) }
       it 'タスクが編集できる' do
         login_as(user)
-        visit edit_task_path(task_created)
+        visit edit_task_path(task)
         fill_in 'task_title', with: 'title_updated'
         click_button 'Update Task'
         expect(page).to have_content 'Task was successfully updated.'
@@ -43,9 +41,8 @@ RSpec.describe 'Tasks', type: :system do
       end
     end
     context 'ログインしていない状態' do
-      let(:task_created){ create(:task) }
       it 'タスクの編集に遷移できない' do
-        visit edit_task_path(task_created)
+        visit edit_task_path(task)
         expect(page).to have_content 'Login required'
         expect(current_path).to eq login_path
       end
@@ -53,16 +50,14 @@ RSpec.describe 'Tasks', type: :system do
   end
   describe 'タスクの削除画面' do
     context 'ログインした状態' do
-      let(:user) { create(:user) }
-      let(:task_created){ create(:task, user_id: user.id) }
       it 'タスクが削除できる' do
         login_as(user)
-        task_created
+        task
         visit tasks_path
         click_on 'Destroy'
         page.driver.browser.switch_to.alert.accept
         expect(page).to have_content 'Task was successfully destroyed.'
-        expect{Task.find(task_created.id)}.to raise_exception(ActiveRecord::RecordNotFound)
+        expect{Task.find(task.id)}.to raise_exception(ActiveRecord::RecordNotFound)
       end
     end
     context 'ログインしていない状態' do
@@ -74,8 +69,6 @@ RSpec.describe 'Tasks', type: :system do
   end
   describe 'タスクの詳細画面' do
     context 'ログインした状態' do
-      let(:user) { create(:user) }
-      let(:task){ create(:task) }
       it 'タスクの詳細に遷移できる' do
         login_as(user)
         visit task_path(task)
@@ -83,7 +76,6 @@ RSpec.describe 'Tasks', type: :system do
       end
     end
     context 'ログインしていない状態' do
-      let(:task){ create(:task) }
       it 'タスクの詳細に遷移できる' do
         visit task_path(task)
         expect(page).to have_content task.title
@@ -91,8 +83,6 @@ RSpec.describe 'Tasks', type: :system do
     end
   end
   describe 'タスクの詳細画面' do
-    let(:user) { create(:user) }
-    let(:task){ create(:task, user_id: user.id) }
     context 'ログインした状態' do
       it 'マイページにユーザーが新規作成したタスクが表示される' do
         login_as(user)
@@ -102,9 +92,6 @@ RSpec.describe 'Tasks', type: :system do
         expect(page).to have_content task.status
       end
     end
-    let(:user) { create(:user) }
-    let(:user_another) { create(:user, email: 'another@gmail.com') }
-    let(:task){ create(:task, user_id: user.id) }
     context 'ログインしていない状態' do
       it '他のユーザーのタスク編集ページへの遷移ができない' do
         login_as(user_another)
